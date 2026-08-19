c68349e33e7b04951dc6628c1337e40d7e74b0a5
Bug-Slayer-404 <BugSlayer404@outlook.com>
2026-08-20 07:31:18 +0800

    UPDATE

---
+++ include/Game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+   private:
+    void Update();
+    void Draw();
+
+   public:
+    void Run();
+};
+
+#endif
---
+++ src/Game.cpp
@@ -0,0 +1,33 @@
+#include "Game.h"
+
+#include <raylib.h>
+
+#define WindowWidth 1280
+#define Windowlength 720
+#define WindowTitle "Roguelike"
+
+void Game::Run() {
+    InitWindow(WindowWidth, Windowlength, WindowTitle);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        Update();
+        Draw();
+    }
+
+    CloseWindow();
+}
+
+void Game::Update() {}
+
+void Game::Draw() {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawRectangle(100, 100, 50, 50, RED);
+    DrawText("Success", 40, 40, 40, PINK);
+
+    EndDrawing();
+}
